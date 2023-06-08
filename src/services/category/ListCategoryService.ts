ef1f2bb2a9d8218150236d25ab149a63cd99cf39
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-06-07 23:08:03 -0300

    refactor: Service refactoring of list categories

--- src/services/category/ListCategoryService.ts
+++ src/services/category/ListCategoryService.ts
@@ -2,7 +2,12 @@ import prismaClient from '../../prisma'
 
 export class ListCategoryService {
   async execute() {
-    const categories = await prismaClient.category.findMany()
+    const categories = await prismaClient.category.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
 
     return categories
   }
