9d7c2fc11f622d913741d9cd9589fd49ee6a222f
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2023-06-09 18:20:34 -0300

    refactor: Refactoring of FinishOrderController

--- src/controllers/order/FinishOrderController.ts
+++ src/controllers/order/FinishOrderController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express'
 import { FinishOrderService } from '../../services/order/FinishOrderService'
+
 export class FinishOrderController {
   async handle(req: Request, res: Response) {
     const orderId = req.query.orderId as string
+
+    if (!orderId || orderId === '') {
+      throw new Error('Order id is required')
+    }
 
     const finishOrderService = new FinishOrderService()
 
